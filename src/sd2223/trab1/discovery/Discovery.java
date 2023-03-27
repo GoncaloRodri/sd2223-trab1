359d32b65bb6ecbc3cd841f8e55f777773cf4a41
barbara-correia <barbaracc2010@gmail.com>
2023-03-27 09:18:13 +0100

    :hammer: discovery introduced

---
+++ src/sd2223/trab1/discovery/Discovery.java
@@ -0,0 +1,162 @@
+package sd2223.trab1.discovery;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * <p>A class interface to perform service discovery based on periodic 
+ * announcements over multicast communication.</p>
+ * 
+ */
+
+public interface Discovery {
+
+	/**
+	 * Used to announce the URI of the given service name.
+	 * @param serviceName - the name of the service
+	 * @param serviceURI - the uri of the service
+	 */
+	public void announce(String serviceName, String serviceURI);
+
+	/**
+	 * Get discovered URIs for a given service name
+	 * @param serviceName - name of the service
+	 * @param minReplies - minimum number of requested URIs. Blocks until the number is satisfied.
+	 * @return array with the discovered URIs for the given service name.
+	 */
+	public URI[] knownUrisOf(String serviceName, int minReplies);
+
+	/**
+	 * Get the instance of the Discovery service
+	 * @return the singleton instance of the Discovery service
+	 */
+	public static Discovery getInstance() {
+		return DiscoveryImpl.getInstance();
+	}
+}
+
+/**
+ * Implementation of the multicast discovery service
+ */
+class DiscoveryImpl implements Discovery {
+	
+	private static Logger Log = Logger.getLogger(Discovery.class.getName());
+
+	// The pre-aggreed multicast endpoint assigned to perform discovery.
+
+	static final int DISCOVERY_RETRY_TIMEOUT = 5000;
+	static final int DISCOVERY_ANNOUNCE_PERIOD = 1000;
+
+	// Replace with appropriate values...
+	static final InetSocketAddress DISCOVERY_ADDR = new InetSocketAddress("239.255.255.255", 9000);
+
+	// Used separate the two fields that make up a service announcement.
+	private static final String DELIMITER = "\t";
+
+	private static final int MAX_DATAGRAM_SIZE = 65536;
+
+	private static Discovery singleton;
+
+	private Dictionary<String,List<URI>> dict = new Hashtable<String,List<URI>>();
+
+	synchronized static Discovery getInstance() {
+		if (singleton == null) {
+			singleton = new DiscoveryImpl();
+		}
+		return singleton;
+	}
+
+	private DiscoveryImpl() {
+		this.startListener();
+	}
+
+	@Override
+	public void announce(String serviceName, String serviceURI) {
+		Log.info(String.format("Starting Discovery announcements on: %s for: %s -> %s\n", DISCOVERY_ADDR, serviceName,
+				serviceURI));
+
+		var pktBytes = String.format("%s%s%s", serviceName, DELIMITER, serviceURI).getBytes();
+		var pkt = new DatagramPacket(pktBytes, pktBytes.length, DISCOVERY_ADDR);
+
+		// start thread to send periodic announcements
+		new Thread(() -> {
+			try (var ds = new DatagramSocket()) {
+				while (true) {
+					try {
+						ds.send(pkt);
+						Thread.sleep(DISCOVERY_ANNOUNCE_PERIOD);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}).start();
+	}
+
+
+	@Override
+	public URI[] knownUrisOf(String serviceName, int minEntries) {
+		List<URI> urisList = null;
+		do {
+			urisList = dict.get(serviceName);
+		}
+		while (urisList == null);
+		while(urisList.size() < minEntries) {
+			return (URI[]) urisList.toArray();	
+		}
+		return (URI[]) urisList.toArray();
+	}
+
+	private void startListener() {
+		Log.info(String.format("Starting discovery on multicast group: %s, port: %d\n", DISCOVERY_ADDR.getAddress(),
+				DISCOVERY_ADDR.getPort()));
+
+		new Thread(() -> {
+			try (var ms = new MulticastSocket(DISCOVERY_ADDR.getPort())) {
+				ms.joinGroup(DISCOVERY_ADDR, NetworkInterface.getByInetAddress(InetAddress.getLocalHost()));
+				for (;;) {
+					try {
+						var pkt = new DatagramPacket(new byte[MAX_DATAGRAM_SIZE], MAX_DATAGRAM_SIZE);
+						ms.receive(pkt);
+
+						var msg = new String(pkt.getData(), 0, pkt.getLength());
+						Log.info(String.format("Received: %s", msg));
+
+						var parts = msg.split(DELIMITER);
+						if (parts.length == 2) {
+							// TODO: complete by storing the decoded announcements...
+							// usar um dicionário para guardar e dps dar return no knownUrisOf
+							var serviceName = parts[0];
+							var uri = URI.create(parts[1]);
+							if(dict.get(serviceName) == null) {
+								List<URI> uris = new ArrayList<URI>();
+								uris.add(uri);
+								dict.put(serviceName, uris);
+							}
+							else {
+								dict.get(serviceName).add(uri);
+							}
+						}
+
+					} catch (Exception x) {
+						x.printStackTrace();
+					}
+				}
+			} catch (Exception x) {
+				x.printStackTrace();
+			}
+		}).start();
+	}
+}
